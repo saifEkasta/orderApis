e48a578202bae8aa65c20820d87605c8f7493d4d
Varun <varun@ekasta.com>
2020-09-22 12:59:12 +0530

    done separate for each file store

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -8,7 +8,7 @@
 
         var order = new Promise((resolve, reject) => {
             
-            const stream = fs.createWriteStream('./test.txt',{flags:'a'});
+            const stream = fs.createWriteStream('./orderCreate.txt',{flags:'a'});
         stream.write(JSON.stringify(req.body) +"\r\n ",function(){
             result = {status:1,data:'success'};
             
@@ -18,6 +18,52 @@
     return order;
     }
 
+
+    
+    function writeSendOrderData(req){
+        var result = {status:0,data:'failed'};
+
+        var order = new Promise((resolve, reject) => {
+            
+            const stream = fs.createWriteStream('./sendOrder.txt',{flags:'a'});
+        stream.write(JSON.stringify(req.body) +"\r\n ",function(){
+            result = {status:1,data:'success'};
+            
+        return resolve(result);
+        })
+        });
+    return order;
+    }
+
+    function writeCatalogData(req){
+        var result = {status:0,data:'failed'};
+
+        var order = new Promise((resolve, reject) => {
+            
+            const stream = fs.createWriteStream('./createCatalog.txt',{flags:'a'});
+        stream.write(JSON.stringify(req.body) +"\r\n ",function(){
+            result = {status:1,data:'success'};
+            
+        return resolve(result);
+        })
+        });
+    return order;
+    }
+    
+    function writeSyncInventoryData(req){
+        var result = {status:0,data:'failed'};
+
+        var order = new Promise((resolve, reject) => {
+            
+            const stream = fs.createWriteStream('./syncInventory.txt',{flags:'a'});
+        stream.write(JSON.stringify(req.body) +"\r\n ",function(){
+            result = {status:1,data:'success'};
+            
+        return resolve(result);
+        })
+        });
+    return order;
+    }
 
 
     var makeOrder = async function(req,res){
@@ -37,7 +83,62 @@
 
     }
 
+    var createCatalog = async function(req,res){
+        var result = {status:0,data:'failed'};
+        var code = 500;
+        var message = 'failed';
+        
+    var writeCatalogResult = await writeCatalogData(req);
+    if(writeCatalogResult.status ==1){
+    message='successfull';
+    code = 200;
+    }
+        
+    res.writeHead(code, message, {'content-type' : 'application/json'});
+    res.end(JSON.stringify(writeCatalogResult));
+
+    }
+
+
+    
+
+    var sendOrder = async function(req,res){
+
+        var result = {status:0,data:'failed'};
+        var code = 500;
+        var message = 'failed';
+        
+    var writeSendOrderResult = await writeSendOrderData(req);
+    if(writeSendOrderResult.status ==1){
+    message='successfull';
+    code = 200;
+    }
+        
+    res.writeHead(code, message, {'content-type' : 'application/json'});
+    res.end(JSON.stringify(writeSendOrderResult));
+
+    }
+    var syncInventory = async function(req,res){
+
+        var result = {status:0,data:'failed'};
+        var code = 500;
+        var message = 'failed';
+        
+    var writeSyncInventoryResult = await writeSyncInventoryData(req);
+    if(writeSyncInventoryResult.status ==1){
+    message='successfull';
+    code = 200;
+    }
+        
+    res.writeHead(code, message, {'content-type' : 'application/json'});
+    res.end(JSON.stringify(writeSyncInventoryResult));
+
+    }
+    
     module.exports = {
-        'makeOrder': makeOrder
+        'makeOrder': makeOrder,
+        'createCatalog':createCatalog,
+        'sendOrder':sendOrder,
+        'syncInventory':syncInventory
         
     }
--- Route/orderApi.js
+++ Route/orderApi.js
@@ -4,7 +4,11 @@ var orderRoute = require('./orderRoute');
 
 module.exports = function(app){	
 
-	app.use('/orders',orderRoute);
-	app.post('/makeOrder',OrderController.makeOrder);  
+	// app.use('/orders',orderRoute)/;
+	app.post('/makeOrder',OrderController.makeOrder); 
+
+	app.post('/createCatalog',OrderController.createCatalog);
+	app.post('/sendOrder',OrderController.sendOrder);
+	app.post('/syncInventory',OrderController.syncInventory);
 
 }
